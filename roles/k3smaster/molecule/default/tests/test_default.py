c4919b9258c40810f1bdb9ff9eaf5c5323f640d3
Matthew Buhagiar <mbuhagiar@Tbcbank.com.ge>
2019-08-07 19:05:20 +0400

    inital commit for molecule-vagrant tests

---
+++ roles/k3smaster/molecule/default/tests/test_default.py
@@ -0,0 +1,27 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+def test_k3s_binary_installed(host):
+        k3s_binary = host.file("/usr/local/bin/k3s").exists
+
+        assert k3s_binary
+    
+    
+def test_k3s_svc(host):
+        k3s_svc = host.service("k3s")
+    
+        assert k3s_svc.is_running
+        assert k3s_svc.is_enabled
